532d7615be48118133556754889a9a60ac4fe25d
Nazia Shehnaz Joynab <ncoder76@gmail.com>
2022-10-03 21:24:43 +0600

    final restructuring

--- cervivorbd/lib/Utils/Exports/screens.dart
+++ cervivorbd/lib/Utils/Exports/screens.dart
@@ -1,6 +1,5 @@
 export 'package:cervivorbd/Screens/TabPages/my_appointments_screen.dart';
 export 'package:cervivorbd/Screens/TabPages/bottom_navigation_menu.dart';
-export 'package:cervivorbd/Screens/TabPages/forum_screen.dart';
 export 'package:cervivorbd/Screens/TabPages/home_screen.dart';
 
 export 'package:cervivorbd/Screens/Authentication/signup_screen.dart';
@@ -11,10 +10,10 @@ export 'package:cervivorbd/Screens/UserProfiles/Patient/patientProfile.dart';
 
 export 'package:cervivorbd/Screens/Forum/Comment/comment.dart';
 export 'package:cervivorbd/Screens/Forum/Comment/comment_container.dart';
-
 export 'package:cervivorbd/Screens/Forum/Post/post.dart';
 export 'package:cervivorbd/Screens/Forum/Post/post_container.dart';
 export 'package:cervivorbd/Screens/Forum/Post/create_post_container.dart';
+export 'package:cervivorbd/Screens/Forum/forum_tab_page.dart';
 
 export 'package:cervivorbd/Screens/CancerInformationModule/intro_to_cc.dart';
 export 'package:cervivorbd/Screens/CancerInformationModule/cc_intro_grid_menu.dart';
@@ -28,3 +27,4 @@ export 'package:cervivorbd/Screens/UserProfiles/Doctor/top_doctor_card.dart';
 
 export 'package:cervivorbd/Screens/Appointment/my_all_appointments.dart';
 export 'package:cervivorbd/Screens/Appointment/my_pending_appointments.dart';
+
